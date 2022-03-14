af9a5e7a15ce5e7579702fbe7401a097e7f0ad8b
Matthew Hu <64753034+wagabooga@users.noreply.github.com>
2022-03-14 03:55:11 -0700

    save to push online

---
+++ frontend/src/components/Navbar/LeftPanelBase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hanabar from './Hanabar/Hanabar'
+import LeftHero from './LeftHero/LeftHeroButton'
+function Navbar() {
+  return <div className="Navbar">
+    <Hanabar />
+    <LeftHero />
+  </div>
+}
+
+export default Navbar
